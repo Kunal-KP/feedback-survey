dc1359f069f65a96414e8a64bc1d63d696d90883
kunal-kp <kunalpanigrahi2000@gmail.com>
2018-01-21 22:12:51 +0530

    oauth and routing complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import Header from './Header';
-const Landing = () => <h2>Landing</h2>;
+import Landing from './Landing';
+
+const Dashboard = () => <h2>Dashboard</h2>;
 
 class App extends Component {
   componentDidMount() {
@@ -16,6 +18,7 @@ class App extends Component {
           <div className="container">
             <Header />
             <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
           </div>
         </BrowserRouter>
       </div>
