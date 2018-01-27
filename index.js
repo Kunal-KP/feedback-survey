e7a062853c30daea66b7c7fd06e36537012d91af
kunal-kp <kunalpanigrahi2000@gmail.com>
2018-01-27 18:50:18 +0530

    script for heroku

--- index.js
+++ index.js
@@ -21,8 +21,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-authRoutes(app);
+//authRoutes(app);
+require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  //To serve the main.js or main.css
+  app.use(express.static('client/build'));
+  //To serve the index.html
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server started on port: ', PORT));
