ef24f8c6ad10ca89ae78ffeaaa3a7708c80e14f5
kunal-kp <kunalpanigrahi2000@gmail.com>
2018-01-27 14:17:03 +0530

    billing impl done

--- client/src/index.js
+++ client/src/index.js
@@ -16,3 +16,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+console.log('Stripe key: ', process.env.REACT_APP_STRIPE_KEY);
+console.log('envt: ', process.env.NODE_ENV);
